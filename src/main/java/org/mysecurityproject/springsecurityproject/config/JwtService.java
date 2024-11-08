11d7c3d4878e9ffdf1f521e8d214abf2502be023
KMaleesha <maleeshakumarasinghe@gmail.com>
2024-11-08 14:11:35 +0530

    create user entity

---
+++ src/main/java/org/mysecurityproject/springsecurityproject/config/JwtService.java
@@ -0,0 +1,10 @@
+package org.mysecurityproject.springsecurityproject.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUsername(String token) {
+        return null;
+    }
+}
